f9c1527a5266db1347adf44d9b3cf2befeef620e
Petr Svoboda <svobodpe@gmail.com>
2025-11-15 12:38:32 +0100

    keep only celsius

--- main.py
+++ main.py
@@ -8,13 +8,10 @@ dhtDevice = adafruit_dht.DHT11(board.D4)
 while True:
     try:
         # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
+        temperature = dhtDevice.temperature
         humidity = dhtDevice.humidity
         print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
+            f"Temp: {temperature:.1f} C    Humidity: {humidity}% "
         )
 
     except RuntimeError as error:
