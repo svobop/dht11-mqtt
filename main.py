a5ea615d5feac14a965e5a10ddcd8f49208129f6
Petr Svoboda <svobodpe@gmail.com>
2025-11-15 12:43:53 +0100

    add logging

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
 import board
 import adafruit_dht
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
@@ -10,18 +14,18 @@ while True:
         # Print the values to the serial port
         temperature = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print(
+        logging.info(
             f"Temp: {temperature:.1f} C    Humidity: {humidity}% "
         )
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
+        logging.error(error.args[0])
         time.sleep(2.0)
         continue
     except Exception as error:
         dhtDevice.exit()
+        logging.exception("An unexpected error occurred:")
         raise error
 
     time.sleep(2.0)
-
